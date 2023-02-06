f96a584a2a1ef11da64ab2c23bcbafa4e97f9cbb
Iman Abbasi <i.abbasi@wallex.net>
2023-02-06 16:47:35 +0330

    Permutation & Combination class

--- src/Combination.php
+++ src/Combination.php
@@ -10,6 +10,21 @@ class Combination
     private bool $repetitions = false;
 
     /**
+     * @var array
+     */
+    private array $items = [];
+
+    /**
+     * @var int
+     */
+    private int $selection = 0;
+
+    /**
+     * @var array
+     */
+    private array $possibilities = [];
+
+    /**
      * @return bool
      */
     public function canRepetitions(): bool
@@ -19,9 +34,81 @@ class Combination
 
     /**
      * @param bool $repetitions
+     * @return Combination
      */
-    public function setRepetitions(bool $repetitions): void
+    public function setRepetitions(bool $repetitions): static
     {
         $this->repetitions = $repetitions;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @return int
+     */
+    public function countItems(): int
+    {
+        return count( $this->getItems() );
+    }
+
+    /**
+     * @param array $items
+     * @return Combination
+     */
+    public function setItems(array $items): static
+    {
+        $this->items = $items;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSelection(): int
+    {
+        return $this->selection;
+    }
+
+    /**
+     * @param int $selection
+     * @return Combination
+     */
+    public function setSelection(int $selection): static
+    {
+        $this->selection = $selection;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPossibilities(): array
+    {
+        return $this->possibilities;
+    }
+
+    /**
+     * @return int
+     */
+    public function countPossibilities(): int
+    {
+        return count( $this->getPossibilities() );
+    }
+
+    /**
+     * @return $this
+     */
+    public function calculate(): static
+    {
+        $this->possibilities = [];
+
+        return $this;
     }
 }
